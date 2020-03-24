06e7aceb4eec49d1a99d1de36c1fb03b0efba56a
iumehara <iumehara10@gmail.com>
2020-03-24 17:24:15 +0900

    initial commit

---
+++ server/src/test/java/com/example/cqrsexample/CqrsexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cqrsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
